bfd6bd991206492ebfbd71f7336b61478a5f8723
sapna.rana <sapna.rana@billo.life>
2022-02-09 15:54:12 +0100

    Add project files.

---
+++ BlobSledgeIssue/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlobSledgeIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pointns = GetListofPoints();
+            pointns.Insert(0, 0);
+            pointns.Add(0);
+            Console.WriteLine("Please Enter the starting point or press enter button to get the Optimal starting point");
+            int startingPoint;
+            if (int.TryParse(Console.ReadLine(), out startingPoint))
+            {
+                TheFarthestDistance(startingPoint, pointns);
+            }
+            else
+            {
+                
+
+                TheFarthestDistancestartingPoint(pointns);
+            }
+            
+
+
+        }
+        private static List<int> GetListofPoints()
+        {
+            var listofPoints = new List<int>();
+            var exitValue = 0;
+            do
+            {
+                Console.WriteLine("Enter sledge point or  press 0 to end of the Sledge Points...");
+                exitValue = Convert.ToInt32(Console.ReadLine());
+                if (exitValue == 0)
+                {
+                    break;
+                }
+                listofPoints.Add(exitValue);
+
+            } while (exitValue != 0);
+            return listofPoints;
+        }
+        private static void TheFarthestDistance(int startingPoint, List<int> points)
+        {
+            int stepsToGoLeft = 0;
+            int stepsToGoRight = 0;
+            StringBuilder steptoFollow = new StringBuilder("Bob can travel steps sideways");
+            
+               
+            if (startingPoint + 1 < points.Count && startingPoint - 1 > 0)
+            {
+                
+                if (points[startingPoint + 1] >= points[startingPoint] && points[startingPoint - 1] >= points[startingPoint])
+                {
+                   Console.WriteLine(steptoFollow.Insert(15,0));
+                }
+                else
+                {
+                    if (points[startingPoint] > points[startingPoint + 1])
+                    {
+                        stepsToGoRight += 1;
+                        for (int i = startingPoint+1; i < points.Count-1; i++)
+                        {
+                            if (points[i] > points[i + 1])
+                            {
+                                stepsToGoRight += 1;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (points[startingPoint] > points[startingPoint - 1])
+                    {
+                        stepsToGoLeft += 1;
+                        for (int i = startingPoint-1; i > 0; i--)
+                        {
+                            if (points[i] > points[i - 1])
+                            {
+                                stepsToGoLeft += 1;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Console.WriteLine(steptoFollow.Insert(15,(stepsToGoLeft >= stepsToGoRight ? stepsToGoLeft: stepsToGoRight)));
+                    
+                }
+            }
+            
+           
+            
+            
+        }
+        private static void TheFarthestDistancestartingPoint(List<int> points)
+        {
+            
+                int startingPoint
+        = !points.Any() ? -1 :
+        points
+        .Select((value, index) => new { Value = value, Index = index })
+        .Aggregate((a, b) => (a.Value > b.Value) ? a : b)
+        .Index;
+                int stepsToGoLeft = 0;
+                int stepsToGoRight = 0;
+               
+                
+                StringBuilder steptoFollow = new StringBuilder("Bob can travel steps sideways");
+
+
+                if (startingPoint + 1 < points.Count && startingPoint - 1 > 0)
+
+                {
+                    int actualstartpoint = startingPoint;
+                    int leftstartingPoint = startingPoint;
+                    for (int star=1; star < startingPoint; star++)
+                    {
+                        
+                       if( points[startingPoint - star] == points[startingPoint] && actualstartpoint != 0)
+                        {
+                            actualstartpoint = startingPoint - star;
+                        }
+                       else
+                        {
+                            break;
+                        }
+                    }
+               
+                List<int> ListRight = points.GetRange(startingPoint, points.Count - startingPoint);
+                List<int> ListLeft = points.GetRange(0, actualstartpoint+1);
+
+                    int steptogonextleft = 0;
+                    int leftstartingPoints = 0;
+                    int steptogonextright = 0;
+                    int rightstartingPoints = 0;
+                for (int i = actualstartpoint; i > 0; i--)
+                {
+                    
+                    if (ListLeft[i] > ListLeft[i - 1])
+                    {
+                        stepsToGoLeft = steptogonextleft;
+                        steptogonextleft += 1;
+                    }
+                    else
+                    {
+
+                        if (steptogonextleft < i && steptogonextleft > stepsToGoLeft)
+                        {
+                            stepsToGoLeft = steptogonextleft;
+                            steptogonextleft = 0;
+                        }
+                    }
+                    leftstartingPoints = i + stepsToGoLeft;
+                    
+                }
+                        
+                      
+                for (int i = 0; i < ListRight.Count-1; i++)
+                {
+
+                    if (ListRight[i] > ListRight[i + 1])
+                    {
+                        stepsToGoRight = steptogonextright;
+                        steptogonextright += 1;
+                        
+
+                    }
+                    else
+                    {
+
+                        if (steptogonextright < ListRight.Count - i && steptogonextright > stepsToGoRight)
+                        {
+                            stepsToGoRight = steptogonextright;
+                            steptogonextright = 0;
+                            
+                        }
+                    }
+                  
+                    rightstartingPoints = startingPoint + stepsToGoRight - i;
+
+                }
+                    
+                    if(stepsToGoLeft >= stepsToGoRight)                    
+                        Console.WriteLine("The starting Index should be" + leftstartingPoints);                    
+                    else
+                        Console.WriteLine("The starting Index should be" + rightstartingPoints);
+                
+            }
+
+        }
+    }
+}
---
+++ UtvecklarBolagetAssignment/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StringComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = string.Empty;
+            string str2 = string.Empty;
+
+            
+            Console.WriteLine("Enter the string Compare To:");
+            str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the string Compare With:");
+            str2 = Console.ReadLine();
+
+            Console.WriteLine("1.Is Equal");
+            Console.WriteLine("2.Index of Deviation");
+            Console.WriteLine("3.String Contains");
+            
+            Console.Write("Enter Choice(1-3):");
+            int ch = Int32.Parse(Console.ReadLine());
+            
+            switch (ch)
+            {
+                case 1:
+                    if (String.Equals(str1, str2))
+                        Console.WriteLine($"{str1} and {str2} have same value.");
+                    else
+                        Console.WriteLine($"{str1} and {str2} are different.");
+                    break;
+                    
+                case 2:
+                    findFirstDifIndex(str1, str2);
+                    break;
+                case 3:
+
+                    CommanCharacter(str1, str2);
+
+                    break;
+                
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+        private static void findFirstDifIndex(string s1, string s2)
+        {
+            for (int i = 0; i < Math.Min(s1.Length, s2.Length); i++)
+                if (s1[i] != s2[i])
+                {
+                    Console.WriteLine($"{s1} and {s2} have an index of deviation at position {i}");
+                    break;
+                }
+
+                else
+                    Console.WriteLine($"{s1} and {s2} have an same value of length {s1.Length}");
+            
+                
+        }
+        private static void CommanCharacter(string str1, string str2)
+        {
+            var common = str1.Intersect(str2).OrderBy(c => c);
+
+            StringBuilder strCommonChar = new StringBuilder();
+            foreach (var c in common)
+            {
+                strCommonChar.Append(',').Append(c);
+
+            }
+            strCommonChar.Remove(0, 1);
+            Console.WriteLine(strCommonChar);
+
+
+        }
+
+    }
+}
